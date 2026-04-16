546028e6de5f16cf67c349aa6ab038c5175d32c2
ali_marwana <ali.marwana@gabera.software>
2026-04-16 14:58:47 +0100

    Add project files.

---
+++ HNG14_Backend_Task1/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿namespace HNG14_Backend_Task1.Models
+{
+    public class Profile
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public float GenderProbability { get; set; }
+        public int SampleSize{ get; set; }
+        public int Age { get; set; }
+        public string AgeGroup { get; set; }
+        public string CountryId { get; set; }
+        public float CountryProbability { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ HNG14_Backend_Task1/Program.cs
@@ -0,0 +1,52 @@
+
+using System.Text.Json;
+
+namespace HNG14_Backend_Task1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers()
+                    .AddJsonOptions(options =>
+                    {
+                        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+                    }); ;
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    // THIS IS WHERE YOU SET Access-Control-Allow-Origin: *
+                    policy.AllowAnyOrigin()   // <-- Sets the header to "*"
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+            app.UseCors("AllowAll");   // <-- Apply the CORS policy
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
